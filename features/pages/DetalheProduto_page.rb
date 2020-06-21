5f3d4067786ce1da4cc2d332fde2207e7be219c1
Daniel Henrique Murbach <danielmurbach@hotmail.com>
2020-06-20 23:28:19 -0300

    Ajustes

--- features/pages/DetalheProduto_page.rb
+++ features/pages/DetalheProduto_page.rb
@@ -2,15 +2,12 @@ class DetalheProduto
     include Capybara::DSL
 
     def SelTamanho
-        find("#group_1")
+        find("#group_1", :from => "M")
     end 
     def SelQtdProd
         find("#quantity_wanted")
     end 
     def AdicionarProd
         find("#add-to-cart-or-refresh > div.product-add-to-cart > div > div.add > button")
-    end 
-    def SelProdCamisa1
-        find("#content > section > div > article:nth-child(1) > div > a > img")
-    end 
+    end   
 end 
---
+++ features/step_definitions/RealizarCompraUsuarioUsado.rb
@@ -0,0 +1,2 @@
+#enconding: utf-8
+file = YAML.load_file(File.join(Dir.pwd, "/features/support/fixtures/dados.yaml"))
